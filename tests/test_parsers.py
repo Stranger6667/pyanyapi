6d012755a3eb7059f491bc00e5c7882f1cbe53c8
Dmitry Dygalo <d.dygalo@volsor.com>
2015-09-18 17:43:05 +0200

    Add test for invalid xml in XMLObjectifyParser

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,8 +1,17 @@
 # coding: utf-8
+import pytest
+
 from pyanyapi import XMLObjectifyParser
+from pyanyapi.exceptions import ResponseParseError
 
 
 def test_xml_objectify_parser():
     parsed = XMLObjectifyParser().parse('<xml><test>123</test></xml>')
     assert parsed.test == 123
     assert parsed.not_existing is None
+
+
+def test_xml_objectify_parser_error():
+    parsed = XMLObjectifyParser().parse('<xml><test>123')
+    with pytest.raises(ResponseParseError):
+        parsed.test
