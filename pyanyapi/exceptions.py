d94d0c7b72bb11dd18be15ccb4fe5b3a7f551689
Dmitry Dygalo <d.dygalo@volsor.com>
2015-11-24 12:26:55 +0100

    Fixed `super` call in exception.

--- pyanyapi/exceptions.py
+++ pyanyapi/exceptions.py
@@ -6,6 +6,6 @@ class ResponseParseError(Exception):
     Raises when data can not be parsed with specified parser.
     """
 
-    def __init__(self, message, content):
-        self.message = message
+    def __init__(self, message, content=None):
+        super(ResponseParseError, self).__init__(message)
         self.content = content
