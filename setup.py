7b078af8320de275dac8dcf3136495b80a7e8b9c
Dmitry Dygalo <dadygalo@gmail.com>
2016-08-09 10:06:08 +0200

    New release

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if not JYTHON:
 setup(
     name='pyanyapi',
     url='https://github.com/Stranger6667/pyanyapi',
-    version='0.5.8',
+    version='0.6.0',
     packages=['pyanyapi'],
     license='MIT',
     author='Dmitry Dygalo',
