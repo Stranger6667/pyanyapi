decbb911c94441da00b431319bb7d4b41523437d
Dmitry Dygalo <dadygalo@gmail.com>
2016-09-27 13:18:44 +0200

    Revert some

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ from pyanyapi import __version__
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.4.6'
+# needs_sphinx = '1.4.6'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
